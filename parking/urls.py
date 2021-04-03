ac0c898204768d743b8951d660269750931fc17d
Vamsidhar Reddy <vamsidhar.reddy@grabtaxi.com>
2021-04-04 00:56:34 +0800

    Url formats changed

--- parking/urls.py
+++ parking/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url
 
 urlpatterns = [
     
-    url(r'^park/(.*)/',   'carparking.views.park', name = 'park'),
-    url(r'^unpark/(.*)/', 'carparking.views.unpark', name = 'unpark'),
+    url(r'^park/([a-zA-Z0-9]+)/',   'carparking.views.park', name = 'park'),
+    url(r'^unpark/([a-zA-Z0-9]+)/', 'carparking.views.unpark', name = 'unpark'),
     url(r'^info/',   'carparking.views.getInfo', name = 'getInfo'),
 ]
